09e62cb0f7f32abc6e67dbac7a610be4ebc23dee
Miles Rissler <medwardr@pm.me>
2025-05-20 21:19:56 -0400

    basic layout

---
+++ Form1.Designer.cs
@@ -0,0 +1,454 @@
+﻿namespace MOM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			splitContainer1 = new SplitContainer();
+			tableLayoutPanel1 = new TableLayoutPanel();
+			listView1 = new ListView();
+			textBox1 = new TextBox();
+			label1 = new Label();
+			button1 = new Button();
+			groupBox1 = new GroupBox();
+			tableLayoutPanel2 = new TableLayoutPanel();
+			textBox7 = new TextBox();
+			label7 = new Label();
+			textBox3 = new TextBox();
+			label3 = new Label();
+			label2 = new Label();
+			textBox2 = new TextBox();
+			label4 = new Label();
+			textBox4 = new TextBox();
+			label6 = new Label();
+			label5 = new Label();
+			textBox6 = new TextBox();
+			textBox5 = new TextBox();
+			maskedTextBox1 = new MaskedTextBox();
+			label9 = new Label();
+			label8 = new Label();
+			textBox8 = new TextBox();
+			label10 = new Label();
+			flowLayoutPanel1 = new FlowLayoutPanel();
+			((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+			splitContainer1.Panel1.SuspendLayout();
+			splitContainer1.Panel2.SuspendLayout();
+			splitContainer1.SuspendLayout();
+			tableLayoutPanel1.SuspendLayout();
+			groupBox1.SuspendLayout();
+			tableLayoutPanel2.SuspendLayout();
+			SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			splitContainer1.Dock = DockStyle.Fill;
+			splitContainer1.Font = new Font("Segoe UI", 11F);
+			splitContainer1.Location = new Point(0, 0);
+			splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			splitContainer1.Panel1.Controls.Add(tableLayoutPanel1);
+			splitContainer1.Panel1.Padding = new Padding(3, 3, 0, 3);
+			// 
+			// splitContainer1.Panel2
+			// 
+			splitContainer1.Panel2.Controls.Add(groupBox1);
+			splitContainer1.Panel2.Padding = new Padding(0, 4, 4, 4);
+			splitContainer1.Size = new Size(832, 515);
+			splitContainer1.SplitterDistance = 336;
+			splitContainer1.TabIndex = 0;
+			// 
+			// tableLayoutPanel1
+			// 
+			tableLayoutPanel1.ColumnCount = 3;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel1.Controls.Add(listView1, 0, 1);
+			tableLayoutPanel1.Controls.Add(textBox1, 1, 0);
+			tableLayoutPanel1.Controls.Add(label1, 0, 0);
+			tableLayoutPanel1.Controls.Add(button1, 2, 0);
+			tableLayoutPanel1.Dock = DockStyle.Fill;
+			tableLayoutPanel1.Font = new Font("Segoe UI", 11F);
+			tableLayoutPanel1.Location = new Point(3, 3);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 2;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle());
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.Size = new Size(333, 509);
+			tableLayoutPanel1.TabIndex = 0;
+			// 
+			// listView1
+			// 
+			tableLayoutPanel1.SetColumnSpan(listView1, 3);
+			listView1.Dock = DockStyle.Fill;
+			listView1.Font = new Font("Segoe UI", 11F);
+			listView1.Location = new Point(3, 36);
+			listView1.Margin = new Padding(3, 3, 0, 3);
+			listView1.Name = "listView1";
+			listView1.Size = new Size(330, 470);
+			listView1.TabIndex = 30;
+			listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// textBox1
+			// 
+			textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			textBox1.Font = new Font("Segoe UI", 11F);
+			textBox1.Location = new Point(62, 3);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(215, 27);
+			textBox1.TabIndex = 10;
+			// 
+			// label1
+			// 
+			label1.Anchor = AnchorStyles.Left;
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI", 11F);
+			label1.Location = new Point(3, 6);
+			label1.Name = "label1";
+			label1.Size = new Size(53, 20);
+			label1.TabIndex = 2;
+			label1.Text = "Search";
+			// 
+			// button1
+			// 
+			button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+			button1.Font = new Font("Segoe UI", 11F);
+			button1.Location = new Point(283, 3);
+			button1.Margin = new Padding(3, 3, 0, 3);
+			button1.Name = "button1";
+			button1.Size = new Size(50, 27);
+			button1.TabIndex = 20;
+			button1.Text = "New";
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			groupBox1.Controls.Add(tableLayoutPanel2);
+			groupBox1.Dock = DockStyle.Fill;
+			groupBox1.Font = new Font("Segoe UI", 11F);
+			groupBox1.Location = new Point(0, 4);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Size = new Size(488, 507);
+			groupBox1.TabIndex = 0;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Household";
+			// 
+			// tableLayoutPanel2
+			// 
+			tableLayoutPanel2.ColumnCount = 6;
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.6666718F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.6666679F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.6666679F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.6666679F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.6666679F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.6666679F));
+			tableLayoutPanel2.Controls.Add(textBox7, 4, 5);
+			tableLayoutPanel2.Controls.Add(label7, 4, 4);
+			tableLayoutPanel2.Controls.Add(textBox3, 0, 3);
+			tableLayoutPanel2.Controls.Add(label3, 0, 2);
+			tableLayoutPanel2.Controls.Add(label2, 0, 0);
+			tableLayoutPanel2.Controls.Add(textBox2, 0, 1);
+			tableLayoutPanel2.Controls.Add(label4, 3, 2);
+			tableLayoutPanel2.Controls.Add(textBox4, 3, 3);
+			tableLayoutPanel2.Controls.Add(label6, 0, 4);
+			tableLayoutPanel2.Controls.Add(label5, 2, 4);
+			tableLayoutPanel2.Controls.Add(textBox6, 0, 5);
+			tableLayoutPanel2.Controls.Add(textBox5, 2, 5);
+			tableLayoutPanel2.Controls.Add(maskedTextBox1, 0, 7);
+			tableLayoutPanel2.Controls.Add(label9, 0, 6);
+			tableLayoutPanel2.Controls.Add(label8, 2, 6);
+			tableLayoutPanel2.Controls.Add(textBox8, 2, 7);
+			tableLayoutPanel2.Controls.Add(label10, 0, 8);
+			tableLayoutPanel2.Controls.Add(flowLayoutPanel1, 0, 9);
+			tableLayoutPanel2.Dock = DockStyle.Fill;
+			tableLayoutPanel2.Location = new Point(3, 23);
+			tableLayoutPanel2.Name = "tableLayoutPanel2";
+			tableLayoutPanel2.RowCount = 10;
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle());
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel2.Size = new Size(482, 481);
+			tableLayoutPanel2.TabIndex = 4;
+			// 
+			// textBox7
+			// 
+			textBox7.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(textBox7, 2);
+			textBox7.Font = new Font("Segoe UI", 11F);
+			textBox7.Location = new Point(323, 129);
+			textBox7.Name = "textBox7";
+			textBox7.PlaceholderText = "USA";
+			textBox7.Size = new Size(156, 27);
+			textBox7.TabIndex = 60;
+			textBox7.Text = "USA";
+			// 
+			// label7
+			// 
+			label7.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label7, 2);
+			label7.Font = new Font("Segoe UI", 11F);
+			label7.Location = new Point(323, 106);
+			label7.Name = "label7";
+			label7.Size = new Size(60, 20);
+			label7.TabIndex = 10;
+			label7.Text = "Country";
+			// 
+			// textBox3
+			// 
+			textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(textBox3, 3);
+			textBox3.Font = new Font("Segoe UI", 11F);
+			textBox3.Location = new Point(3, 76);
+			textBox3.Name = "textBox3";
+			textBox3.PlaceholderText = "129 Pleasant Valley Rd.";
+			textBox3.Size = new Size(234, 27);
+			textBox3.TabIndex = 20;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label3, 3);
+			label3.Font = new Font("Segoe UI", 11F);
+			label3.Location = new Point(3, 53);
+			label3.Name = "label3";
+			label3.Size = new Size(48, 20);
+			label3.TabIndex = 3;
+			label3.Text = "Street";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label2, 6);
+			label2.Font = new Font("Segoe UI", 11F);
+			label2.Location = new Point(3, 0);
+			label2.Name = "label2";
+			label2.Size = new Size(49, 20);
+			label2.TabIndex = 1;
+			label2.Text = "Name";
+			// 
+			// textBox2
+			// 
+			textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(textBox2, 6);
+			textBox2.Font = new Font("Segoe UI", 11F);
+			textBox2.Location = new Point(3, 23);
+			textBox2.Name = "textBox2";
+			textBox2.Size = new Size(476, 27);
+			textBox2.TabIndex = 10;
+			textBox2.TextChanged += textBox2_TextChanged;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label4, 3);
+			label4.Font = new Font("Segoe UI", 11F);
+			label4.Location = new Point(243, 53);
+			label4.Name = "label4";
+			label4.Size = new Size(34, 20);
+			label4.TabIndex = 4;
+			label4.Text = "City";
+			// 
+			// textBox4
+			// 
+			textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(textBox4, 3);
+			textBox4.Font = new Font("Segoe UI", 11F);
+			textBox4.Location = new Point(243, 76);
+			textBox4.Name = "textBox4";
+			textBox4.PlaceholderText = "East Earl";
+			textBox4.Size = new Size(236, 27);
+			textBox4.TabIndex = 30;
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label6, 2);
+			label6.Font = new Font("Segoe UI", 11F);
+			label6.Location = new Point(3, 106);
+			label6.Name = "label6";
+			label6.Size = new Size(69, 20);
+			label6.TabIndex = 8;
+			label6.Text = "ZIP Code";
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label5, 2);
+			label5.Font = new Font("Segoe UI", 11F);
+			label5.Location = new Point(163, 106);
+			label5.Name = "label5";
+			label5.Size = new Size(43, 20);
+			label5.TabIndex = 6;
+			label5.Text = "State";
+			// 
+			// textBox6
+			// 
+			textBox6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(textBox6, 2);
+			textBox6.Font = new Font("Segoe UI", 11F);
+			textBox6.Location = new Point(3, 129);
+			textBox6.Name = "textBox6";
+			textBox6.PlaceholderText = "17519";
+			textBox6.Size = new Size(154, 27);
+			textBox6.TabIndex = 40;
+			// 
+			// textBox5
+			// 
+			textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(textBox5, 2);
+			textBox5.Font = new Font("Segoe UI", 11F);
+			textBox5.Location = new Point(163, 129);
+			textBox5.Name = "textBox5";
+			textBox5.PlaceholderText = "PA";
+			textBox5.Size = new Size(154, 27);
+			textBox5.TabIndex = 50;
+			textBox5.Text = "PA";
+			// 
+			// maskedTextBox1
+			// 
+			maskedTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(maskedTextBox1, 2);
+			maskedTextBox1.Location = new Point(3, 182);
+			maskedTextBox1.Mask = "(999) 000-0000";
+			maskedTextBox1.Name = "maskedTextBox1";
+			maskedTextBox1.Size = new Size(154, 27);
+			maskedTextBox1.TabIndex = 70;
+			// 
+			// label9
+			// 
+			label9.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label9, 2);
+			label9.Font = new Font("Segoe UI", 11F);
+			label9.Location = new Point(3, 159);
+			label9.Name = "label9";
+			label9.Size = new Size(50, 20);
+			label9.TabIndex = 14;
+			label9.Text = "Phone";
+			// 
+			// label8
+			// 
+			label8.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label8, 4);
+			label8.Font = new Font("Segoe UI", 11F);
+			label8.Location = new Point(163, 159);
+			label8.Name = "label8";
+			label8.Size = new Size(46, 20);
+			label8.TabIndex = 12;
+			label8.Text = "Email";
+			// 
+			// textBox8
+			// 
+			textBox8.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(textBox8, 4);
+			textBox8.Font = new Font("Segoe UI", 11F);
+			textBox8.Location = new Point(163, 182);
+			textBox8.Name = "textBox8";
+			textBox8.Size = new Size(316, 27);
+			textBox8.TabIndex = 80;
+			// 
+			// label10
+			// 
+			label10.AutoSize = true;
+			tableLayoutPanel2.SetColumnSpan(label10, 2);
+			label10.Font = new Font("Segoe UI", 11F);
+			label10.Location = new Point(3, 212);
+			label10.Name = "label10";
+			label10.Size = new Size(71, 20);
+			label10.TabIndex = 81;
+			label10.Text = "Members";
+			// 
+			// flowLayoutPanel1
+			// 
+			flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.SetColumnSpan(flowLayoutPanel1, 6);
+			flowLayoutPanel1.Location = new Point(3, 245);
+			flowLayoutPanel1.Name = "flowLayoutPanel1";
+			flowLayoutPanel1.Size = new Size(476, 233);
+			flowLayoutPanel1.TabIndex = 82;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.FromArgb(243, 243, 243);
+			ClientSize = new Size(832, 515);
+			Controls.Add(splitContainer1);
+			Font = new Font("Segoe UI", 11F);
+			Margin = new Padding(4);
+			Name = "Form1";
+			Text = "Membership Office Manager";
+			splitContainer1.Panel1.ResumeLayout(false);
+			splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+			splitContainer1.ResumeLayout(false);
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel1.PerformLayout();
+			groupBox1.ResumeLayout(false);
+			tableLayoutPanel2.ResumeLayout(false);
+			tableLayoutPanel2.PerformLayout();
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private SplitContainer splitContainer1;
+		private TableLayoutPanel tableLayoutPanel1;
+		private TextBox textBox1;
+		private Label label1;
+		private Button button1;
+		private ListView listView1;
+		private GroupBox groupBox1;
+		private Label label2;
+		private TextBox textBox2;
+		private Label label3;
+		private TextBox textBox3;
+		private TableLayoutPanel tableLayoutPanel2;
+		private TextBox textBox4;
+		private Label label4;
+		private Label label5;
+		private TextBox textBox7;
+		private Label label7;
+		private TextBox textBox5;
+		private Label label6;
+		private TextBox textBox6;
+		private MaskedTextBox maskedTextBox1;
+		private Label label8;
+		private Label label9;
+		private TextBox textBox8;
+		private Label label10;
+		private FlowLayoutPanel flowLayoutPanel1;
+	}
+}
