9b77e2bdf128b88565e560be52f73ddadef4a011
Miles Rissler <medwardr@pm.me>
2025-11-27 10:42:25 -0500

    Update v

--- App/Program.cs
+++ App/Program.cs
@@ -5,7 +5,7 @@ namespace MOM
 {
     internal static class Program
     {
-		public static Version Version { get => Version.Parse("1.0.5"); }
+		public static Version Version { get => Version.Parse("1.0.6"); }
 
 		private static frmHouseholds? _mainForm;
 
