d4a7e1ce372ef2e20c68e2bdad2197b27278f6dc
Miles Rissler <medwardr@pm.me>
2026-08-03 21:50:03 -0400

    Nuget stuff etc

--- App/Program.cs
+++ App/Program.cs
@@ -1,97 +1,96 @@
 using MOM.Forms;
 using Serilog;
 
-namespace MOM
+namespace MOM;
+
+internal static class Program
 {
-    internal static class Program
-    {
-		public static Version Version { get => Version.Parse("1.2.1"); }
+	public static Version Version { get => Version.Parse("1.2.1"); }
 
-		private static frmHouseholds? _mainForm;
+	private static frmHouseholds? _mainForm;
 
-		[STAThread]
-        private static void Main(string[] args)
-        {
-			ApplicationConfiguration.Initialize();
+	[STAThread]
+	private static void Main(string[] args)
+	{
+		ApplicationConfiguration.Initialize();
 
-			if (args.Contains("tools", StringComparer.OrdinalIgnoreCase))
-			{
-				Application.Run(new frmTools());
-			}
-			else
-			{
-				Application.ThreadException += (s, e) =>
-				{
-					HandleException(e.Exception, "An unhandled exception occurred");
-				};
-				AppDomain.CurrentDomain.UnhandledException += (s, e) =>
-				{
-					HandleException(e.ExceptionObject as Exception, "An unhandled exception occurred");
-				};
-
-				InitializeLogger();
-				Log.Information("Application start");
-
-				Application.EnableVisualStyles();
-				Application.SetCompatibleTextRenderingDefault(false);
-
-				_mainForm = new frmHouseholds();
-				Application.Run(_mainForm);
-				_mainForm.LogOut();
-
-				Log.Information("Application close");
-				CloseLogger();
-			}
-        }
-
-		private static void InitializeLogger()
+		if (args.Contains("tools", StringComparer.OrdinalIgnoreCase))
 		{
-			var logFile = GetLogFile();
-			var loggerConfiguration = new LoggerConfiguration();
-			loggerConfiguration.MinimumLevel.Debug();
-			loggerConfiguration.WriteTo.File(logFile);
-			Log.Logger = loggerConfiguration.CreateLogger();
+			Application.Run(new frmTools());
+		}
+		else
+		{
+			Application.ThreadException += (s, e) =>
+			{
+				HandleException(e.Exception, "An unhandled exception occurred");
+			};
+			AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+			{
+				HandleException(e.ExceptionObject as Exception, "An unhandled exception occurred");
+			};
+
+			InitializeLogger();
+			Log.Information("Application start");
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			_mainForm = new frmHouseholds();
+			Application.Run(_mainForm);
+			_mainForm.LogOut();
+
+			Log.Information("Application close");
+			CloseLogger();
+		}
+	}
+
+	private static void InitializeLogger()
+	{
+		var logFile = GetLogFile();
+		var loggerConfiguration = new LoggerConfiguration();
+		loggerConfiguration.MinimumLevel.Debug();
+		loggerConfiguration.WriteTo.File(logFile);
+		Log.Logger = loggerConfiguration.CreateLogger();
+	}
+
+	public static void CloseLogger()
+	{
+		Log.CloseAndFlush();
+	}
+
+	private static string GetLogFile()
+	{
+		return GetSavedFile(Path.Combine("logs", $"log-{DateTime.Now:yyyy-MM-dd}.txt"));
+	}
+
+	public static string GetSavedFile(string fileName)
+	{
+		string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+		return Path.Combine(localAppData, "MOM", fileName);
+	}
+
+	private static void HandleException(Exception? ex, string context)
+	{
+		bool exit;
+
+		Log.Error(ex, context);
+		try
+		{
+			using var frm = new frmError(ex);
+			frm.ShowDialog();
+			exit = frm.ExitProgram;
+		}
+		catch (Exception e)
+		{
+			Log.Fatal(e, "An error occurred while displaying the error form");
+			exit = true;
 		}
 
-		public static void CloseLogger()
+		if (exit)
 		{
-			Log.CloseAndFlush();
-		}
-
-		private static string GetLogFile()
-		{
-			return GetSavedFile(Path.Combine("logs", $"log-{DateTime.Now:yyyy-MM-dd}.txt"));
-		}
-
-		public static string GetSavedFile(string fileName)
-		{
-			string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-			return Path.Combine(localAppData, "MOM", fileName);
-		}
-
-		private static void HandleException(Exception? ex, string context)
-		{
-			bool exit;
-
-			Log.Error(ex, context);
-			try
-			{
-				using var frm = new frmError(ex);
-				frm.ShowDialog();
-				exit = frm.ExitProgram;
-			}
-			catch (Exception e)
-			{
-				Log.Fatal(e, "An error occurred while displaying the error form");
-				exit = true;
-			}
-			
-			if (exit)
-			{
-				_mainForm?.LogOut();
-				CloseLogger();
-				Environment.Exit(1);
-			}
+			_mainForm?.LogOut();
+			CloseLogger();
+			Environment.Exit(1);
 		}
 	}
 }
